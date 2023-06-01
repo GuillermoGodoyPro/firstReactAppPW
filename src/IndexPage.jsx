8ca546ad850fb8b39862e647e2f118086976fa88
guillermogodoypro <guillermo.rgo92@gmail.com>
2023-06-01 00:36:26 -0300

    update page

--- src/IndexPage.jsx
+++ src/IndexPage.jsx
@@ -3,18 +3,37 @@ import React from 'react'
 export const IndexPage = () => {
   return (
     <div>
+        
         <nav className='nav-div'>
             <a href=" ">
                 <div className='nav-wrapper App-logo'>
                 </div>
-                <span className='logo' >Carrito</span>
-            </a>
-        </nav>
+                <h1 className='title' >Elije tu Ropa</h1>
+            </a>                    
+        </nav>        
+        
         <main>
             <div className='search-box-wrapper'>
                 <input type='text' placeholder='Buscar..' className='search-box'/>
             </div>
+            <div className='list-wrapper'>
+                <section className='list'>
+                    <a href=" " className='list-item'> 
+                        <img src="https://img.freepik.com/foto-gratis/vista-gorra-camionero-equipo-beisbol_23-2149410056.jpg?w=740&t=st=1685587241~exp=1685587841~hmac=a3aace6a5b1a0a95e36b673ede7cef0285bf41dc2bcfee6d4f5d302ee9d5015d" alt="ropaHombre"/> 
+                    </a>
+                    <div>
+                        <p>Ropa hombre</p>
+                    </div>
+                    <a href=" " className='list-item'> 
+                        <img src="https://img.freepik.com/foto-gratis/moda-rapida-vs-moda-lenta-sostenible_23-2149133976.jpg?w=740&t=st=1685587288~exp=1685587888~hmac=302caba7f690f38aa6959f6c5d5394918ba3650fff6f43a2ed4237b10453429d" alt="remera" />
+                    </a>
+                    <div>
+                        <p>Remera Trols</p>
+                    </div>
+                </section>
+            </div>
         </main>
+        
     </div>
   )
 }
