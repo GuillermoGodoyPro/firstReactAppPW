e513daf38e0eb1e167b8c0dcadf4fdb8a66e7613
guillermogodoypro <guillermo.rgo92@gmail.com>
2023-06-10 00:19:35 -0300

    Update page

--- src/IndexPage.jsx
+++ src/IndexPage.jsx
@@ -33,6 +33,12 @@ export const IndexPage = () => {
                             <p>Ropa hombre</p>
                         </div>
                         <a href=" " className='list-item'> 
+                            <img src="https://img.freepik.com/psd-gratis/camiseta-maqueta-diseno_15879-478.jpg?size=626&ext=jpg&uid=R80410374&ga=GA1.2.1853624699.1685586449&semt=sph" alt="RemeraMujer"/> 
+                        </a>
+                        <div>
+                            <p>Remera Mujer</p>
+                        </div>
+                        <a href=" " className='list-item'> 
                             <img src="https://img.freepik.com/foto-gratis/moda-rapida-vs-moda-lenta-sostenible_23-2149133976.jpg?w=740&t=st=1685587288~exp=1685587888~hmac=302caba7f690f38aa6959f6c5d5394918ba3650fff6f43a2ed4237b10453429d" alt="remera" />
                         </a>
                         <div>
